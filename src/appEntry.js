e30e0be29db9b622a140f5dadde6b227c0015991
Aleksandar Vučenić <aleksandar.vucenic@gmail.com>
2019-04-30 19:00:42 +0200

    Add bootstap and webpack

---
+++ src/appEntry.js
@@ -0,0 +1,2 @@
+import 'bootstrap';
+import './appEntry.scss';
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/appEntry.js',
+    output: {
+        path: path.resolve(__dirname),
+        filename: './App.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(scss)$/,
+            use: [
+              {
+                // Adds CSS to the DOM by injecting a `<style>` tag
+                loader: 'style-loader'
+              },
+              {
+                // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                loader: 'css-loader'
+              },
+              {
+                // Loader for webpack to process CSS with PostCSS
+                loader: 'postcss-loader',
+                options: {
+                  plugins: function () {
+                    return [
+                      require('autoprefixer')
+                    ];
+                  }
+                }
+              },
+              {
+                // Loads a SASS/SCSS file and compiles it to CSS
+                loader: 'sass-loader'
+              }
+            ]
+          }
+        ]
+      }
+}

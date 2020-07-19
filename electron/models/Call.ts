e384df71134296359d9dafce945561e56a1b2fcc
Bojan Čoka <coka@nsoss.org>
2020-07-19 16:16:35 +0200

    Explicitly set join column names

--- electron/models/Call.ts
+++ electron/models/Call.ts
@@ -2,6 +2,7 @@ import {
     BaseEntity,
     Column,
     Entity,
+    JoinColumn,
     ManyToOne,
     PrimaryGeneratedColumn,
 } from 'typeorm';
@@ -27,53 +28,62 @@ export default class Call extends BaseEntity {
         type => Volunteer,
         volunteer => volunteer.calls
     )
+    @JoinColumn({ name: 'volunteer_id' })
     volunteer: Volunteer;
 
     @ManyToOne(
         type => CallOrdinality,
         callOrdinality => callOrdinality.calls
     )
+    @JoinColumn({ name: 'call_ordinality_id' })
     callOrdinality: CallOrdinality;
 
     @ManyToOne(
         type => CallType,
         callType => callType.calls
     )
+    @JoinColumn({ name: 'call_type_id' })
     callType: CallType;
 
     @ManyToOne(
         type => Gender,
         gender => gender.calls
     )
+    @JoinColumn({ name: 'gender_id' })
     gender: Gender;
 
     @ManyToOne(
         type => MaritalStatus,
         maritalStatus => maritalStatus.calls
     )
+    @JoinColumn({ name: 'marital_status_id' })
     maritalStatus: MaritalStatus;
 
     @ManyToOne(
         type => PostCallState,
         postCallState => postCallState.calls
     )
+    @JoinColumn({ name: 'post_call_state_id' })
     postCallState: PostCallState;
 
     @ManyToOne(
         type => ProblemType,
         problemType => problemType.calls
     )
+    @JoinColumn({ name: 'problem_type_id' })
     problemType: ProblemType;
 
     @ManyToOne(
         type => SuicideFactor,
         suicideFactor => suicideFactor.calls
     )
+    @JoinColumn({ name: 'suicide_factor_id' })
     suicideFactor: SuicideFactor;
 
     @ManyToOne(
         type => SuicideRisk,
         suicideRisk => suicideRisk.calls
     )
+    @JoinColumn({ name: 'suicide_risk_id' })
     suicideRisk: SuicideRisk;
 }
